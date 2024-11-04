6f94b9104649109ff5913d468a85b24fe91ac1b9
FabriDamazio <fabridamazio@gmail.com>
2024-11-04 10:35:22 -0300

    keymap to open terminal

--- lua/fabri/remap.lua
+++ lua/fabri/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- open terminal
+vim.keymap.set("n", "<leader>t", "<cmd>:terminal<CR>")
+
 -- Move selected blocks up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
