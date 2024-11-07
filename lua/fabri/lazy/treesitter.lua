dbabdd1c1fb948abf3cc50b24de2ad97d784c296
FabriDamazio <fabridamazio@gmail.com>
2024-11-07 12:42:09 -0300

    Add roslyn lsp for csharp

--- lua/fabri/lazy/treesitter.lua
+++ lua/fabri/lazy/treesitter.lua
@@ -6,7 +6,8 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "elixir", "eex", "heex", "erlang"
+                "jsdoc", "bash", "elixir", "eex", "heex", "erlang", "c_sharp",
+                "godot_resource", "gdscript"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
