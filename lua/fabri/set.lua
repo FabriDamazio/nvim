989fbd720df6db7156617de6c0fca0162851dd26
FabriDamazio <fabridamazio@gmail.com>
2024-11-10 22:32:34 -0300

    Change cursor and disable relative lines

--- lua/fabri/set.lua
+++ lua/fabri/set.lua
@@ -1,7 +1,6 @@
-vim.opt.guicursor = ""
-
+vim.opt.guicursor = "i:block-blinkwait100-blinkoff400-blinkon250"
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
