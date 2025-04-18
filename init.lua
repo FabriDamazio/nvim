dd8ab1955a5dfdfecb8af074df5180b975d7c42c
FabriDamazio <fabridamazio@gmail.com>
2025-04-18 12:29:48 -0300

    Lazy vim config

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+
+-- Set the default colorscheme
+vim.cmd.colorscheme "tokyonight-night"
+
+-- source the file.
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+-- executes the current line.
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+-- executes the selected lines
+vim.keymap.set("v", "<space>x", ":lua<CR>")
