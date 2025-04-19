b7858d2b38ef2deedf8f680f65e9664b88958255
FabriDamazio <fabridamazio@gmail.com>
2025-04-18 22:10:35 -0300

    some keymaps

--- init.lua
+++ init.lua
@@ -1,15 +1,7 @@
-vim.g.mapleader = " "       -- Set <Leader> to <Space>
-vim.g.maplocalleader = " "  -- Optional: Also set <LocalLeader> to <Space>
-
+require("config.keymaps")
 require("config.lazy")
 require("config.options")
 
 -- Set the default colorscheme
 vim.cmd.colorscheme "tokyonight-night"
 
--- source the file.
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
--- executes the current line.
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
--- executes the selected lines
-vim.keymap.set("v", "<space>x", ":lua<CR>")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,81 @@
+-- Leader key setup
+vim.g.mapleader = "\\"
+
+-- File Explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {
+  desc = "[P]roject [V]iew (File Explorer)"
+})
+
+-- Terminal
+vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR>", {
+  desc = "Open [T]erminal"
+})
+
+-- Window Resize
+vim.keymap.set("n", "<leader>r", "20<C-w>_", {
+  desc = "[R]esize window (20 lines)"
+})
+
+-- Move Lines (Visual Mode)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {
+  desc = "Move line down"
+})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {
+  desc = "Move line up"
+})
+
+-- System Clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], {
+  desc = "[Y]ank to system clipboard"
+})
+vim.keymap.set("n", "<leader>Y", [["+Y]], {
+  desc = "[Y]ank line to system clipboard"
+})
+
+-- Delete to Void
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], {
+  desc = "[D]elete to void (no yank)"
+})
+
+-- Formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {
+  desc = "[F]ormat buffer (LSP)"
+})
+
+-- Find and Replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
+  desc = "[S]ubstitute word under cursor"
+})
+
+-- Fun Effects
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", {
+  desc = "[M]ake it [R]ain (fun effect)"
+})
+
+-- Reload Config
+vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end, {
+  desc = "Reload Neovim config"
+})
+
+-- Execute Lua
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", {
+  desc = "E[x]ecute current line as Lua"
+})
+vim.keymap.set("v", "<leader>x", ":lua<CR>", {
+  desc = "E[x]ecute selection as Lua"
+})
+
+-- Diagnostics
+vim.keymap.set({ "n", "i", "v" }, "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {
+  desc = "Show diagnostic [E]rrors"
+})
+
+-- Select All
+vim.keymap.set("n", "<leader>a", "ggVG", {
+  desc = "[A]ll (Select entire buffer)"
+})
+
+-- New Tab
+vim.keymap.set("n", "<C-t>", "<Cmd>tabnew<CR>", {
+  desc = "New [T]ab"
+})
